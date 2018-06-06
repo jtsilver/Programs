69899d6f91e51c07f7d5a8419b63c489eeef96a2
jtsilver <39391042+jtsilver@users.noreply.github.com>
2018-06-06 10:01:40 -0400

    Update guess.js

--- guess.js
+++ guess.js
@@ -1,18 +1,32 @@
-alert('Guess the number between 1-100 within 20 tries')
+console.log('Guess the number between 1-100 within 15 tries');
+let prompt = require('prompt');
+prompt.start();
 
 const answer = Math.floor(Math.random()*100);
-var guess = prompt('Enter your guess:');
+let guess = prompt.get('guess', function (err, result) {
+    console.log(' Guess: ' + result.guess);
+  });
+console.log('What is your guess?');
+guess;
 
-for (i=0;i<20;i++) {
+for (let i=0;i<15;i++) {
   if (guess == answer) {
-    alert('YOU WIN!');
-    alert('You guessed in ' + (i+1) + ' tries!');
+    console.log('YOU WIN!');
+    console.log('You guessed in ' + (i+1) + ' tries!');
     break;
   }
   else if (guess > answer) {
-    guess = prompt('Too high. You have ' + (20-i) + ' tries remaining! Try again:')
+    console.log('Too high. You have ' + (15-i) + ' tries remaining! Try again:');
+    guess = prompt.get('guess', function (err, result) {
+    console.log(' Guess: ' + result.guess);
+    });
+    guess;
   }
   else if (guess < answer) {
-    guess = prompt('Too low. You have ' + (20-i) + ' tries reamining! Try again:')
+    console.log('Too low. You have ' + (15-i) + ' tries reamining! Try again:');
+    guess = prompt.get('guess', function (err, result) {
+    console.log(' Guess: ' + result.guess);
+    });
+    guess;
   }
 }
