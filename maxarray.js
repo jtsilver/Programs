658ab228b14743be3bddb8f31b59a9278d1bcaf9
Tanner Silver <jtsilver94@gmail.com>
2018-06-07 10:43:15 -0400

    third

--- maxarray.js
+++ maxarray.js
@@ -1,11 +1,11 @@
 function max (arr) {
   let maxArr = [];
   for (let i = 0; i<arr.length;i++) {
-    if (arr[i]>arr[i-1] || arr[i+1]<arr[i]) {
-      maxArr = [arr[i]]
+    if (arr[i++]<arr[i]) {
+      maxArr = arr[i];
     }
   }
   console.log(maxArr);
 }
 
-max([1,2,3,4,5,6]);
+max([1,2,3,4,5,6,4,9000009]);
