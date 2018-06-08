dcf59112a30797c5260d8b95c98a39dc23b84b9c
Tanner Silver <jtsilver94@gmail.com>
2018-06-08 10:10:37 -0400

    second

--- haystack.js
+++ haystack.js
@@ -1,8 +1,15 @@
 function haystack(arr, str) {
-  for (let i =0;i<arr.length;i++) {
-    let splitArr = arr[i].split('');
-    console.log(splitArr.includes(str));
+  if (str.length == 1) {
+    for (let i =0;i<arr.length;i++) {
+      let splitArr = arr[i].split('');
+      console.log(splitArr.includes(str));
+    }
+  }
+  else if (str.length > 1) {
+    for (let i = 0;i<arr.length;i++) {
+      console.log(arr[i].includes(str));
+    }
   }
 }
 
-haystack(['ppp','aaaaaaaaaaaaaaa','ddddddddddddddddddddde','ffffff'],'e');
+haystack(['ppp','aaaaaaaaaaaaaaa','ddddddddddddddddddddde','ffffff'],'de');
